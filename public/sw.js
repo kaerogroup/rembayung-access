2b40311b167272ca63b9d8ecef35e95f9ee31546
kaerogroup <kaerogroup@gmail.com>
2026-09-04 18:04:45 +0800

    fix(pwa): restore Android installability (#8)
    * fix(pwa): add Chromium-compatible PNG install icons
    
    * fix(pwa): use explicit PNG installability icons
    
    * fix(pwa): advertise raster install icons
    
    * fix(pwa): cache raster install assets

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'rembayung-shell-v1'
-const APP_SHELL = ['/', '/offline.html', '/manifest.webmanifest', '/icon.svg', '/maskable-icon.svg']
+const CACHE_NAME = 'rembayung-shell-v2'
+const APP_SHELL = ['/', '/offline.html', '/manifest.webmanifest', '/icon-192.png', '/icon-512.png']
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)))
